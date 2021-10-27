ed9e580c9a745f4e8f61a07daab20b3174f46982
Reza Fitriaman <rezafitriaman@pop-os.localdomain>
2021-10-27 23:10:53 +0200

    finished 24 and finished assignment 1

---
+++ the-basics/my-basics-project/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.scss']
+  styles: [
+    `h3 {
+      color: red
+    }`
+  ]
+})
+export class AppComponent {
+  title = 'my-basics-project-master';
+}
