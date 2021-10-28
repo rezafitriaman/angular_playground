080a89379c179b1b732f519dff5218cb045fabcd
Reza Fitriaman <rezafitriaman@pop-os.localdomain>
2021-10-28 23:42:39 +0200

    finished 35

--- the-basics/my-basics-project/src/app/servers/servers.component.ts
+++ the-basics/my-basics-project/src/app/servers/servers.component.ts
@@ -4,14 +4,40 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]',
   //selector: '.app-servers',
   selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl: 'servers.component.html',
+  //templateUrl: `<app-server></app-server><app-server></app-server>`,
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
+  btnText: string = 'add mastermind';
+  serverName: string = 'TestServer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit(): void {
   }
 
+  onMouseover() {
+    this.btnText = 'yes master!'
+  }
+
+  onMouseout() {
+    this.btnText = 'add mastermind!'
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    let targetElm = event.target as HTMLInputElement;
+    this.serverName = targetElm.value;
+  }
 }
