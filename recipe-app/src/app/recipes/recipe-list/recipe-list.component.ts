df732085910670b41c2bdac018a382ecf0c4f942
Reza Fitriaman <rezafitriaman@pop-os.localdomain>
2021-11-02 23:29:49 +0100

    finished 55

---
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = [
+    new Recipe('A test recipe','This is a simply a test','https://www.foodiesfeed.com/wp-content/uploads/2021/01/fried-egg-and-guacamole-sandwiches.jpg'),
+    new Recipe('A test recipe','This is a simply a test','https://www.foodiesfeed.com/wp-content/uploads/2021/01/fried-egg-and-guacamole-sandwiches.jpg')
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
