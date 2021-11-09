1e76ce893d391972f6a026c2c3b9fd678a5bfa35
Reza Fitriaman <rezafitriaman@pop-os.localdomain>
2021-11-09 21:18:02 +0100

    finished 87

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -6,6 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+/*  recipeNav: boolean;
+  shoppingListNav: boolean;*/
+  loadedFeature = 'recipes';
 
+  constructor() {
+/*    this.recipeNav = true;
+    this.shoppingListNav = false;*/
+  }
+  onNavigate(feature: string) {
+    console.log(event);
+    this.loadedFeature = feature;
+  }
+/*  recipeHasClicked(event: boolean) {
+    if(event) {
+      console.log('recipeHasClicked');
+      this.recipeNav = true;
+      this.shoppingListNav = false;
+    }
+  }
+  shoppingListClicked(event: boolean) {
+      if(event) {
+        console.log('shoppingListClicked');
+        this.recipeNav = false;
+        this.shoppingListNav = true;
+      }
+  }*/
 }
 
--- recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {Recipe} from "../../recipe.model";
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.scss']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input('recipeItem') recipe: Recipe;
 
-  constructor() { }
+  constructor() {
+    this.recipe = {
+      name: '',
+      description: '',
+      imagePath: ''
+    }
+  }
+
 
   ngOnInit(): void {
   }
