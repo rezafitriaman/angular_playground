d442054f313dd7559405626fb85b4b2df4c732f5
Reza Fitriaman <rezafitriaman@pop-os.localdomain>
2022-01-18 19:27:44 +0100

    add template form type

---
+++ forms-example/src/app/app.component.ts
@@ -0,0 +1,59 @@
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm | undefined
+  title = 'forms-example';
+  defaultQuestions = 'teacher';
+  answer = '';
+  genders = ['male', 'female']
+  user = {
+    username: '',
+    email: '',
+    secretQuestions: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
+  constructor() {
+  }
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    /*this.signupForm?.setValue({
+      userData: {
+        username: suggestedName,
+        email: '',
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    })*/
+
+    this.signupForm?.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+/*  onSubmit(form: NgForm) {
+    console.log('submitted', form)
+  }*/
+  onSubmit() {
+    this.submitted = true;
+    console.log('submitted', this.signupForm)
+    this.user.username = this.signupForm?.value.userData.username;
+    this.user.email = this.signupForm?.value.userData.email;
+    this.user.secretQuestions = this.signupForm?.value.secret;
+    this.user.answer = this.signupForm?.value.questionAnswer;
+    this.user.gender = this.signupForm?.value.gender;
+
+     this.signupForm?.reset();
+  }
+}
