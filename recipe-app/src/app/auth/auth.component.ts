9e29cc77186a910f1d89743bed5d2042beb70626
Reza Fitriaman <reza@rezas-mbp.kpn>
2022-02-06 20:07:03 +0100

    295

--- recipe-app/src/app/auth/auth.component.ts
+++ recipe-app/src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
-
+  isLoginMode = true;
+  
+  constructor(private authService: AuthService) { }
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) return;
+
+    console.log(form.value);
+    if (this.isLoginMode) {
+      console.log('u are on login mode')
+    } else {
+      console.log('u are on signup mode')
+      this.authService.signup(form.value.email, form.value.password).subscribe(restData => {
+        console.log(restData);
+      });
+    }
+    form.reset()
   }
 
 }
