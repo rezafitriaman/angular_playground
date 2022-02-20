274ddd2ae8e74fff7c380818f4c2da52943ad426
Reza Fitriaman <reza.fitriaman@quli.nl>
2022-02-20 20:15:04 +0100

    change color blue to sky

--- todoApp/tailwind.config.js
+++ todoApp/tailwind.config.js
@@ -1,5 +1,17 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [
-    './src/**/*.{html,ts}'
-  ],
-}
+	content: ['./src/**/*.{html,js,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				green: colors.emerald,
+				yellow: colors.amber,
+				purple: colors.violet,
+				gray: colors.neutral,
+			},
+		},
+	},
+	plugins: [],
+};
