9010739644ea6c80a3fae73baf032d900be96b9a
Reza Fitriaman <reza.fitriaman@quli.nl>
2022-02-20 21:08:55 +0100

    angular upgrade to 13, tailwind upgrade to 3

--- todoApp/tailwind.config.js
+++ todoApp/tailwind.config.js
@@ -1,17 +1,7 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-	content: ['./src/**/*.{html,js,ts}'],
-
+	content: ['./src/**/*.{html,ts}'],
 	theme: {
-		extend: {
-			colors: {
-				green: colors.emerald,
-				yellow: colors.amber,
-				purple: colors.violet,
-				gray: colors.neutral,
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
