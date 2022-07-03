8a9712535f5c6b47c6eb20a116c35b225821a7d3
Reza Fitriaman <reza.fitriaman@quli.nl>
2022-07-03 18:34:45 +0200

    #111-add-todo-item-to-firebase - add loading spinner with tailwind

--- todoApp/src/app/shared/loading-spinner/loading-spinner.component.ts
+++ todoApp/src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -2,12 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-loading-spinner',
-    template: `<div class="lds-ring">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>`,
+    template: `<div class="lds-ring inline-block w-4 h-4 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
+                    <div class="box-border block absolute w-6 h-6 m-0 border-4 border-rose-400 rounded-full border-t-rose-400 border-r-transparent border-b-transparent border-l-transparent animate-spin "></div>
+                    <div class="box-border block absolute w-6 h-6 m-0 border-4 border-rose-400 rounded-full border-t-rose-400 border-r-transparent border-b-transparent border-l-transparent animate-spin animation-delay-500"></div>
+                    <div class="box-border block absolute w-6 h-6 m-0 border-4 border-rose-400 rounded-full border-t-rose-400 border-r-transparent border-b-transparent border-l-transparent animate-spin animation-delay-300"></div>
+                    <div class="box-border block absolute w-6 h-6 m-0 border-4 border-rose-400 rounded-full border-t-rose-400 border-r-transparent border-b-transparent border-l-transparent animate-spin animation-delay-150"></div>
+                 </div>`,
     styleUrls: ['./loading-spinner.component.css']
 })
 export class LoadingSpinnerComponent {}
